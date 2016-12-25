4f82a7ad2443d16729fe2ead8be09b3d20f4c9dd
inu1255 <929909260@qq.com>
2016-12-26 00:01:34 +0800

    first commit

---
+++ string.go
@@ -0,0 +1,42 @@
+package gohelper
+
+import (
+	"regexp"
+	"strings"
+	"unsafe"
+)
+
+var (
+	re_hump *regexp.Regexp
+)
+
+func humpStrFunc(from string) string {
+	if len(from) == 2 {
+		return strings.ToUpper(from[1:])
+	}
+	return strings.ToUpper(from)
+}
+
+func HumpStr(s string) string {
+	if re_hump == nil {
+		re_hump = regexp.MustCompile(`([-_]|^)[a-z]`)
+	}
+	return re_hump.ReplaceAllStringFunc(s, humpStrFunc)
+}
+
+func UpperFirstLetter(s string) string {
+	if len(s) > 0 {
+		return strings.ToUpper(s[:1]) + s[1:]
+	}
+	return s
+}
+
+func Str2bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func Bytes2str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
---
+++ struct.go
@@ -0,0 +1,61 @@
+package gohelper
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+)
+
+func Json2struct(s interface{}, classname string) error {
+	var info map[string]interface{}
+	switch v := s.(type) {
+	case string:
+		err := json.Unmarshal(Str2bytes(v), &info)
+		if err != nil {
+			return err
+		}
+	case map[string]interface{}:
+		info = v
+	default:
+		return errors.New("unknow")
+	}
+	var f func(interface{}) string
+	f = func(value interface{}) string {
+		switch v := value.(type) {
+		case bool:
+			return "bool\n"
+		case int:
+			return "int\n"
+		case int8:
+			return "int8\n"
+		case int16:
+			return "int16\n"
+		case int32:
+			return "int32\n"
+		case int64:
+			return "int64\n"
+		case float32:
+			return "float32\n"
+		case float64:
+			return "float64\n"
+		case string:
+			return "string\n"
+		case []interface{}:
+			if len(v) > 0 {
+				return "[]" + f(v[0])
+			}
+			return "[]interface{}\n"
+		case map[string]interface{}:
+			s := " struct{\n"
+			for k, value := range v {
+				s += UpperFirstLetter(k) + " " + f(value)
+			}
+			s += "}\n"
+			return s
+		default:
+			return "interface{}\n"
+		}
+	}
+	ioutil.WriteFile(classname+".go", Str2bytes("package main\ntype "+UpperFirstLetter(classname)+" "+f(info)), 0655)
+	return nil
+}
