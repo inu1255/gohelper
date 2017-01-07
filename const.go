2cb2d139405f4d81916dd5e1c4765d9476e6461e
inu1255 <929909260@qq.com>
2017-01-07 15:02:59 +0800

    add iszero

--- const.go
+++ const.go
@@ -6,3 +6,43 @@ func Today() time.Time {
 	t := time.Now().Unix()
 	return time.Unix(t-(t+28800)%86400, 0)
 }
+
+type zeroable interface {
+	IsZero() bool
+}
+
+func IsZero(k interface{}) bool {
+	switch k.(type) {
+	case int:
+		return k.(int) == 0
+	case int8:
+		return k.(int8) == 0
+	case int16:
+		return k.(int16) == 0
+	case int32:
+		return k.(int32) == 0
+	case int64:
+		return k.(int64) == 0
+	case uint:
+		return k.(uint) == 0
+	case uint8:
+		return k.(uint8) == 0
+	case uint16:
+		return k.(uint16) == 0
+	case uint32:
+		return k.(uint32) == 0
+	case uint64:
+		return k.(uint64) == 0
+	case float32:
+		return k.(float32) == 0
+	case float64:
+		return k.(float64) == 0
+	case bool:
+		return k.(bool) == false
+	case string:
+		return k.(string) == ""
+	case zeroable:
+		return k.(zeroable).IsZero()
+	}
+	return false
+}
